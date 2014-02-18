438477eb983d2d031fd979477c6af519a48a4869
takyonxxx <turkaybiliyor@hotmail.com>
2014-02-18 10:47:50 +0200

    Create Sound Frequency
    Create Sound Frequency

---
+++ TurkayWaves/Form1.Designer.cs
@@ -0,0 +1,116 @@
+﻿namespace TurkayWaves
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.hScrollfreq = new System.Windows.Forms.HScrollBar();
+            this.labelfrq = new System.Windows.Forms.Label();
+            this.playtime = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button1.Location = new System.Drawing.Point(9, 41);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(135, 31);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "START";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // hScrollfreq
+            // 
+            this.hScrollfreq.LargeChange = 1;
+            this.hScrollfreq.Location = new System.Drawing.Point(9, 85);
+            this.hScrollfreq.Maximum = 22000;
+            this.hScrollfreq.Name = "hScrollfreq";
+            this.hScrollfreq.Size = new System.Drawing.Size(502, 21);
+            this.hScrollfreq.TabIndex = 1;
+            this.hScrollfreq.Value = 1000;
+            this.hScrollfreq.Scroll += new System.Windows.Forms.ScrollEventHandler(this.hScrollfreq_Scroll);
+            // 
+            // labelfrq
+            // 
+            this.labelfrq.AutoSize = true;
+            this.labelfrq.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.labelfrq.Location = new System.Drawing.Point(213, 121);
+            this.labelfrq.Name = "labelfrq";
+            this.labelfrq.Size = new System.Drawing.Size(112, 25);
+            this.labelfrq.TabIndex = 2;
+            this.labelfrq.Text = "16000 Hz";
+            // 
+            // playtime
+            // 
+            this.playtime.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.playtime.Location = new System.Drawing.Point(150, 41);
+            this.playtime.Name = "playtime";
+            this.playtime.Size = new System.Drawing.Size(59, 31);
+            this.playtime.TabIndex = 3;
+            this.playtime.Text = "2";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(215, 44);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(103, 25);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Seconds";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(520, 169);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.playtime);
+            this.Controls.Add(this.labelfrq);
+            this.Controls.Add(this.hScrollfreq);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.HScrollBar hScrollfreq;
+        private System.Windows.Forms.Label labelfrq;
+        private System.Windows.Forms.TextBox playtime;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ TurkayWaves/Form1.cs
@@ -0,0 +1,182 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Diagnostics;
+using System.IO;
+using System.Runtime.InteropServices;
+namespace TurkayWaves
+{
+    public partial class Form1 : Form
+    {
+        [DllImport("winmm.dll", EntryPoint = "PlaySound", SetLastError = true)]
+        private extern static int PlaySound(byte[] wavData, IntPtr hModule, PlaySoundFlags flags);
+
+        //#define SND_SYNC            0x0000  /* play synchronously (default) */
+        //#define SND_ASYNC           0x0001  /* play asynchronously */
+        //#define SND_NODEFAULT       0x0002  /* silence (!default) if sound not found */
+        //#define SND_MEMORY          0x0004  /* pszSound points to a memory file */
+        //#define SND_LOOP            0x0008  /* loop the sound until next sndPlaySound */
+        //#define SND_NOSTOP          0x0010  /* don't stop any currently playing sound */
+
+        //#define SND_NOWAIT      0x00002000L /* don't wait if the driver is busy */
+        //#define SND_ALIAS       0x00010000L /* name is a registry alias */
+        //#define SND_ALIAS_ID    0x00110000L /* alias is a predefined ID */
+        //#define SND_FILENAME    0x00020000L /* name is file name */
+        //#define SND_RESOURCE    0x00040004L /* name is resource name or atom */
+
+        enum PlaySoundFlags
+        {
+            SND_SYNC = 0x0000,
+            SND_ASYNC = 0x0001,
+            SND_MEMORY = 0x0004
+        }
+
+        // Play a wav file appearing in a byte array
+        static void PlayWav(byte[] wav)
+        {
+            PlaySound(wav, System.IntPtr.Zero, PlaySoundFlags.SND_MEMORY | PlaySoundFlags.SND_SYNC);
+        }
+        static byte[] ConvertSamplesToWavFileFormat(short[] left, short[] right, int sampleRate)
+        {
+            Debug.Assert(left.Length == right.Length);
+
+            const int channelCount = 2;
+            int sampleSize = sizeof(short) * channelCount * left.Length;
+            int totalSize = 12 + 24 + 8 + sampleSize;
+
+            byte[] wav = new byte[totalSize];
+            int b = 0;
+
+            // RIFF header
+            wav[b++] = (byte)'R';
+            wav[b++] = (byte)'I';
+            wav[b++] = (byte)'F';
+            wav[b++] = (byte)'F';
+            int chunkSize = totalSize - 8;
+            wav[b++] = (byte)(chunkSize & 0xff);
+            wav[b++] = (byte)((chunkSize >> 8) & 0xff);
+            wav[b++] = (byte)((chunkSize >> 16) & 0xff);
+            wav[b++] = (byte)((chunkSize >> 24) & 0xff);
+            wav[b++] = (byte)'W';
+            wav[b++] = (byte)'A';
+            wav[b++] = (byte)'V';
+            wav[b++] = (byte)'E';
+
+            // Format header
+            wav[b++] = (byte)'f';
+            wav[b++] = (byte)'m';
+            wav[b++] = (byte)'t';
+            wav[b++] = (byte)' ';
+            wav[b++] = 16;
+            wav[b++] = 0;
+            wav[b++] = 0;
+            wav[b++] = 0; // Chunk size
+            wav[b++] = 1;
+            wav[b++] = 0; // Compression code
+            wav[b++] = channelCount;
+            wav[b++] = 0; // Number of channels
+            wav[b++] = (byte)(sampleRate & 0xff);
+            wav[b++] = (byte)((sampleRate >> 8) & 0xff);
+            wav[b++] = (byte)((sampleRate >> 16) & 0xff);
+            wav[b++] = (byte)((sampleRate >> 24) & 0xff);
+            int byteRate = sampleRate * channelCount * sizeof(short); // byte rate for all channels
+            wav[b++] = (byte)(byteRate & 0xff);
+            wav[b++] = (byte)((byteRate >> 8) & 0xff);
+            wav[b++] = (byte)((byteRate >> 16) & 0xff);
+            wav[b++] = (byte)((byteRate >> 24) & 0xff);
+            wav[b++] = channelCount * sizeof(short);
+            wav[b++] = 0; // Block align (bytes per sample)
+            wav[b++] = sizeof(short) * 8;
+            wav[b++] = 0; // Bits per sample
+
+            // Data chunk header
+            wav[b++] = (byte)'d';
+            wav[b++] = (byte)'a';
+            wav[b++] = (byte)'t';
+            wav[b++] = (byte)'a';
+            wav[b++] = (byte)(sampleSize & 0xff);
+            wav[b++] = (byte)((sampleSize >> 8) & 0xff);
+            wav[b++] = (byte)((sampleSize >> 16) & 0xff);
+            wav[b++] = (byte)((sampleSize >> 24) & 0xff);
+
+            Debug.Assert(b == 44);
+
+            for (int s = 0; s != left.Length; ++s)
+            {
+                wav[b++] = (byte)(left[s] & 0xff);
+                wav[b++] = (byte)(((ushort)left[s] >> 8) & 0xff);
+                wav[b++] = (byte)(right[s] & 0xff);
+                wav[b++] = (byte)(((ushort)right[s] >> 8) & 0xff);
+
+            }
+
+            Debug.Assert(b == totalSize);
+
+            return wav;
+        }
+
+        // Create a simple sine wave
+        public void CreateSamples(out short[] left, out short[] right, int sampleRate,int time)
+        {
+            //const double middleC = 261.626;
+            int count = sampleRate * time; // Two seconds
+            left = new short[count];
+            right = new short[count];
+
+            for (int i = 0; i != count; ++i)
+            {
+               
+                double t = (double)i / sampleRate; // Time of this sample in seconds
+                short s = (short)Math.Floor(Math.Sin(t * 2 * Math.PI * hScrollfreq.Value) * short.MaxValue);
+                left[i] = s;
+                right[i] = s;
+               
+            }           
+           
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            labelfrq.Text = hScrollfreq.Value.ToString() + "Hz";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            short[] left;
+            short[] right;
+            int sampleRate = 44100;
+            button1.Enabled = false;
+            labelfrq.Text = "playing";
+            Application.DoEvents();
+            CreateSamples(out left, out right, sampleRate,Convert.ToInt16(playtime.Text));
+            byte[] wav = ConvertSamplesToWavFileFormat(left, right, sampleRate);
+            PlayWav(wav);
+            labelfrq.Text = hScrollfreq.Value.ToString() + "Hz";
+            button1.Enabled = true;
+            Application.DoEvents();
+           
+            /*
+            // Write the data to a wav file
+            using (FileStream fs = new FileStream(@"C:\documents and settings\carlos\desktop\a440stereo.wav", FileMode.Create))
+            {
+                fs.Write(wav, 0, wav.Length);
+            }
+            */
+        }
+
+        private void hScrollfreq_Scroll(object sender, ScrollEventArgs e)
+        {
+            labelfrq.Text = hScrollfreq.Value.ToString() + "Hz";
+        }
+    }
+}
